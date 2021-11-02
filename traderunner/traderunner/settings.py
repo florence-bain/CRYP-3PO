9b8d777ec51935b78d42bd4c77912ee07a38f026
Osneil Drakes <osneil-drakes@hotmail.com>
2021-11-02 16:19:41 +0000

    update database settings

--- traderunner/traderunner/settings.py
+++ traderunner/traderunner/settings.py
@@ -73,13 +73,24 @@ WSGI_APPLICATION = 'traderunner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'traderunner',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'djangouser',
+        'PASSWORD': 'password',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
