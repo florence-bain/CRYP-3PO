4436564ef20789e696b10592544ed297adccf2ea
Jessica Bulman <jessicabulman97@gmail.com>
2021-11-03 19:24:40 +0000

    changing websocket channels on subscribe_channel method

--- connectors/binance_futures.py
+++ connectors/binance_futures.py
@@ -203,7 +203,7 @@ class BinanceFuturesClient:
     def _on_open(self, ws):
         logger.info("Binance connection opened")
 
-        self.subscribe_channel(self.contracts['BTCUSD'])
+        self.subscribe_channel(list(self.contracts.values()), "bookTicker")
 
     def _on_close(self, ws):
         logger.warning("Binance websocket connection closed")
@@ -227,17 +227,19 @@ class BinanceFuturesClient:
                     self.prices[symbol]['ask'] = float(data['a'])
 
 
-    def subscribe_channel(self, contract: Contract):
+    def subscribe_channel(self, contracts: typing.List[Contract], channel: str):
         data = dict()
         data['method'] = "SUBSCRIBE"
         data['params'] = []
-        data['params'].append(contract.symbol.lower() + "@bookTicker")
+
+        for contract in contracts:
+            data['params'].append(contract.symbol.lower() + "@" + channel)
         data['id'] = self._ws_id
 
         try:
             self._ws.send(json.dumps(data))
         except: Exception as e:
-        logger.error("Websocket error while subscribing to %s: %s", contract.symbol, e)
+        logger.error("Websocket error while subscribing to %s %s updates: %s", log(contracts), channel, e)
 
         self._ws_id += 1
 
