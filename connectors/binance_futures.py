0498002ac32fdaa3ecbc8eaf2da67f88e3ee4e9c
Jessica Bulman <jessicabulman97@gmail.com>
2021-11-03 13:37:28 +0200

    binance futures API public endpoints and testing

--- connectors/binance_futures.py
+++ connectors/binance_futures.py
@@ -4,16 +4,69 @@ import requests
 logger = logging.getLogger()
 
 
-def get_contracts():
+class BinanceFuturesClient:
+    def __init__(self, testnet):
+        if testnet:
+            self.base_url = "https://testnet.binancefuture.com"
+        else:
+            self.base_url = "https://fapi.binance.com"
 
-    response_object = requests.get("https://fapi.binance.com/fapi/v1/exchangeInfo")
+        self.prices = dict()
 
-    contracts = []
+        logger.info('Binance Futures Client Successfully Initialized')
 
-    for contract in response_object.json()['symbols']:
-        contracts.append(contract['pair'])
+    def make_request(self, method, endpoint, data):
+        if method == "GET":
+            response = requests.get(self.base_url + endpoint, params=data)
+        else:
+            raise ValueError()
 
-    return contracts
+        if response.status_code == 200:
+            return response.json()
+        else:
+            logger.error("Error while making %s request to %s: %s (error code %s",
+                         method, endpoint, response.json(), response.status_code)
+            return None
+
+    def get_contracts(self):
+        exchange_info = self.make_request("GET", "/fapi/v1/exchangeInfo", None)
+
+        contracts = dict()
+
+        if exchange_info is not None:
+            for contract_data in exchange_info[ 'symbols']:
+                contracts[contract_data['pair']] = contract_data
+
+        return contracts
+
+    def get_historical_candles(self, symbol, interval):
+        data = dict()
+        data['symbol'] = symbol
+        data['interval'] = interval
+        data['limit'] = 1000
+
+        raw_candles = self.make_request("GET", "/fapi/v1/klines", data)
+
+        candles = []
+
+        if raw_candles is not None:
+            for c in raw_candles:
+                candles.append([c[0], float(c[1]),float(c[2]),float(c[3]),float(c[4]),float(c[5])])
 
 
-print(get_contracts())
+
+        return candles
+
+    def get_bid_ask(self, symbol):
+        data = dict()
+        data['symbol'] = symbol
+        ob_data = self.make_request("GET", "/fapi/v1/ticker/bookTicker", data)
+
+        if ob_data is not None:
+            if symbol not in self.prices:
+                self.prices[symbol] = {'bid': float(ob_data['bidPrice']), 'ask': float(ob_data['askPrice'])}
+            else:
+                self.prices[symbol]['bid'] = float(ob_data['bidPrice'])
+                self.prices[symbol]['ask'] = float(ob_data['askPrice'])
+
+        return self.prices[symbol]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import logging
 
-from connectors.bitmex import get_contracts
+from connectors.binance_futures import BinanceFuturesClient
 
 logger = logging.getLogger()
 
@@ -21,24 +21,8 @@ logger.addHandler(file_handler)
 
 if __name__ == '__main__':
 
-    bitmex_contracts = get_contracts()
+    binance = BinanceFuturesClient(True)
+    print(binance.get_contracts())
 
     root = tk.Tk()
-    root.configure(bg="gray12")
-
-    i = 0
-    j = 0
-
-    calibri_font = ("Calibri", 11, "normal")
-
-    for contract in bitmex_contracts:
-        label_widget = tk.Label(root, text=contract, bg='gray12', fg='SteelBlue1', width=13, font=calibri_font)
-        label_widget.grid(row=i, column=j, sticky='ew')
-
-        if i == 4:
-            j += 1
-            i = 0
-        else:
-            i += 1
-
     root.mainloop()
