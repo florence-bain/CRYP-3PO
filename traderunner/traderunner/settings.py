0acafe360da1b942e66f710efbb15a4667891227
Osneil Drakes <osneil-drakes@hotmail.com>
2021-11-04 12:15:23 +0000

    create Procfile update django for heroku module

--- traderunner/traderunner/settings.py
+++ traderunner/traderunner/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'herokuapp',
+    #'herokuapp',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +140,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+django_heroku.settings(locals())
