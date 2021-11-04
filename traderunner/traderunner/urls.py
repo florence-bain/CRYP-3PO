c2720ad6e06e1f9f7d86a0c92505002cb1a9e9e9
Osneil Drakes <osneil-drakes@hotmail.com>
2021-11-04 11:32:01 +0000

    update django project for homepage

--- traderunner/traderunner/urls.py
+++ traderunner/traderunner/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('register', views.register_request, name="register"),
     path('login', views.login_request, name="login"),
     path('logout', views.logout_request, name= "logout"),
+    path('welcomepage', TemplateView.as_view(template_name='welcomepage.html'), name='welcomepage' )
 ]
