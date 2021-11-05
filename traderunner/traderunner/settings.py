589e6f8a9ebf4bcc70ce67ea615176882e5d89aa
Florence Bain <florence_bain@outlook.com>
2021-11-05 10:48:36 +0000

    updating allowed hosts

--- traderunner/traderunner/settings.py
+++ traderunner/traderunner/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-%($v@kwbdueo4t!it#ixg&_6u6)v1y=^0la0m-#)^yj)qnf__t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
